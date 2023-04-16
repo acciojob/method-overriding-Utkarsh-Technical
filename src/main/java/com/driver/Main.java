391c67946d985265c280efdbd78f69800d833d1a
Utkarsh Shukla <63734211+Utkarsh-Technical@users.noreply.github.com>
2023-04-16 23:23:14 +0530

    Assignment done updated 1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -13,8 +13,7 @@ public class Main {
   }
 
   public static void main(String[] args){
-      B b = new B();
-      String str = b.meth();
-      System.out.println(str);
+
+      System.out.println(new B().meth());
   }
 }
